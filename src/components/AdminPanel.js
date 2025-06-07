e4b88ea4e7f1bd7e8129205174f96a71b3b70dc9
Syam Son Penumala <syamson132000@gmail.com>
2025-06-07 21:54:59 +0530

    Update AdminPanel.js

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,26 +1,16 @@
-import React, { useState } from "react";
-import data from "../data";
+import React from "react";
 
 export default function AdminPanel() {
-  const [products, setProducts] = useState(data);
-
-  const removeProduct = (id) => {
-    setProducts(products.filter((p) => p.id !== id));
-  };
-
   return (
-    <div className="col-12">
+    <div className="container">
       <h1>Admin Panel</h1>
-      {products.map((product) => (
-        <div key={product.id}>
-          <a href="#">
-            <div className="row">
-              {product.name} - ₹{product.price}
-            </div>
-          </a>
-          <button onClick={() => removeProduct(product.id)}>Remove</button>
+      <div className="row">
+        <div className="col-12">
+          <div>
+            <a href="#">Sample Admin Action</a>
+          </div>
         </div>
-      ))}
+      </div>
     </div>
   );
 }
