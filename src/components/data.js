6bf1e42ae7ef0de24b1e73fb38cef5e9cab7705d
Syam Son Penumala <syamson132000@gmail.com>
2025-06-07 20:30:59 +0530

    Create data.js

---
+++ src/components/data.js
@@ -0,0 +1,12 @@
+const initialProducts = [
+  {
+    id: 1,
+    name: "iPhone 13",
+    price: 799,
+    description: "Latest iPhone model",
+    image: "https://via.placeholder.com/150"
+  },
+  // Add 7 more to make 8 initially
+];
+
+export default initialProducts;
